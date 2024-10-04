93975bd1fc5baba0b8ec242d6773dae0d955af5b
Deathvks <dylanjesussuarez@gmail.com>
2024-10-04 12:41:03 +0100

    post enhaced

--- fronted/src/app/add-car/add-car.page.ts
+++ fronted/src/app/add-car/add-car.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CarService } from '../services/car.service';
+import { ToastController } from '@ionic/angular';  // Importar ToastController para retroalimentación visual
 
 @Component({
   selector: 'app-add-car',
@@ -12,8 +13,8 @@ export class AddCarPage implements OnInit {
   carForm: FormGroup;
 
   constructor(private carService: CarService,
-    public formBuider: FormBuilder,
-
+              public formBuider: FormBuilder,
+              private toastController: ToastController  // Inyectar ToastController
   ) { 
     this.carForm = this.formBuider.group({
       brand: ['', Validators.compose([Validators.required])],
@@ -23,8 +24,8 @@ export class AddCarPage implements OnInit {
 
   ngOnInit() { }
 
-  createCar(){
-    if(!this.carForm.valid){
+  createCar() {
+    if (!this.carForm.valid) {
       console.log("It's not valid");
       return;
     }
@@ -32,13 +33,15 @@ export class AddCarPage implements OnInit {
     const brand = this.carForm.value.brand;
     const model = this.carForm.value.model;
 
-    this.carService.create(brand, model).subscribe((response) => {
+    this.carService.create(brand, model).subscribe(async (response) => {
       console.log("Car created");
-    })
+
+      this.carForm.reset();
+
+    });
   }
 
-  getFormControl(field: string){
+  getFormControl(field: string) {
     return this.carForm.get(field);
   }
-
 }
